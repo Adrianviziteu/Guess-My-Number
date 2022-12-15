26f42595f9a71e8b10ebe196b40902b2910fa243
Adrian <adriannviziteu@icloud.com>
2022-12-15 17:28:47 +0200

    Updates in JS

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
 "use strict";
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector(".number").textContent = secretNumber;
+
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
-  console.log(guess, typeof guess);
 
   if (!guess) {
     document.querySelector(".message").textContent = "⛔️ No number typed";
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "🎉 Correct Number!";
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "⬆️ Number to high!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "👻 You lost the game!";
+      document.querySelector(".score").textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "⬇️ Number to low!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "👻 You lost the game!";
+      document.querySelector(".score").textContent = 0;
+    }
   }
 });
